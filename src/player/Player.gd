05e51bcc9bd0890aaa6fc8a4930e0e9a3b1518d8
Felipe Flores <lffm.fismat@gmail.com>
2022-04-03 19:51:06 -0600

    terminado physics engine y documentación básica

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -1,14 +1,19 @@
 extends KinematicBody2D
 
-var velocity = Vector2.ZERO
-#managing constants here
-const MAX_SPEED = 140
-const MAX_ACCEL = 800
-const FRICTION = 400
-const GRAVITY = 600
-const MAX_FALL_SPEED = 200
+const MAX_SPEED = 140 		# pixel / s
+const ACCELERATION = 800    # Funciona igual como fricción
+const GRAVITY = 600			# Permite que la velocidad inter-frame sea GRAVITY / FPS
+const MAX_FALL_SPEED = 250
 const JUMP_SPEED = 60*230
 
+# ===========================================================================================
+# Sean G y JS respectivamente GRAVITY Y JUMP_SPEED. Definamos el impulso de salto, I, como:
+# 	I = (G - JS) / FPS
+# Entonces, el desplazamiento máximo altanzado por un salto es I^2 / (2G), y la duración 
+# completa del salto es 2I/G.
+# ===========================================================================================
+
+var velocity = Vector2.ZERO
 onready var animationPlayer = $AnimationPlayer
 
 func _physics_process(delta):
@@ -19,12 +24,15 @@ func _physics_process(delta):
 
 func jump(delta):
 	if Input.is_action_just_pressed('jump') and is_on_floor():
-		velocity += Vector2.UP * JUMP_SPEED * delta
+		print(velocity)
+		velocity += Vector2.UP * JUMP_SPEED * delta  # Da un impulso de GRAVITY/FPS - JS/FPS = I
+		print(velocity)
 
 func move(delta): 
 	var input = sign(Input.get_action_strength("right") - Input.get_action_strength("left"))
-	velocity.x = move_toward(velocity.x, MAX_SPEED*input, delta*MAX_ACCEL)
+	velocity.x = move_toward(velocity.x, MAX_SPEED*input, delta*ACCELERATION)
 	velocity.y = move_toward(velocity.y, MAX_FALL_SPEED, delta*GRAVITY)
+	print(velocity)
 
 func crouch():
 	pass
@@ -32,9 +40,11 @@ func crouch():
 func animation():
 	if not is_on_floor():
 		animationPlayer.play("jump")
+		if velocity.x != 0:
+			animationPlayer.flip_h = velocity.x < 0 
 	elif velocity.x != 0 :
 		animationPlayer.play("run")
-		animationPlayer.flip_h = velocity.x < 0
+		animationPlayer.flip_h = velocity.x < 0 
 	else:
 		animationPlayer.play("idle")
 
